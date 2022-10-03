fda7c28792e8a28b42c60010bfb1316dd105335d
InterestsFantastic <30320765+InterestsFantastic@users.noreply.github.com>
2022-10-03 13:59:07 -0400

    record_reader created to read in rows of data

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python3
+from copy import copy
 
 def keyval(sheet, *funcs):
     '''For a sheet with rows of keys and values, creates a dictionary from it with functions applied to the keys or keys and values, or none.
@@ -9,10 +10,32 @@ def keyval(sheet, *funcs):
         for row in sheet:
             out[row[0]] = row[1]
     else:
-        print('hi')
         for row in sheet:
             if len(funcs) == 1:
                 out[funcs[0](row[0])] = funcs[0](row[1])
             else:
                 out[funcs[0](row[0])] = funcs[1](row[1])
     return out
+
+# perhaps use namedtuple instead
+def record_reader(sheet, *funcs):
+    '''Outputs a list of dicts from a spreadsheet, accepting functions to change the elements of the dicts.
+    First row is labels and is untouched. If number of elements exceeds the functions provided, the rest are just handled as strings.'''
+    out = []
+    first_row = sheet[0]
+    for row in sheet[1:]:
+        record = {}
+        if not funcs:
+            for i,e in enumerate(row):
+                record[first_row[i]] = e
+        else:
+            for i,e in enumerate(row):
+                if i+1 <= len(funcs):
+                    func = funcs[i]
+                else:
+                    func = str
+                record[first_row[i]] = func(e)
+        out.append(copy(record))
+    return out
+    
+    
