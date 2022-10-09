9e1031ad0ef190a378b87b45e934921b913757fe
InterestsFantastic <30320765+InterestsFantastic@users.noreply.github.com>
2022-10-09 12:19:34 -0400

    Found an error when referring back to odsreader.py

--- utils.py
+++ utils.py
@@ -15,41 +15,48 @@ def keyval(sheet, *funcs):
                 out[funcs[0](row[0])] = funcs[1](row[1])
     return out
 
-# perhaps use namedtuple instead
-def record_reader(sheet, *funcs, nones='fill'):
+def interpret_none(key, row, nones='fill'):
+    '''Enters a value into row[key] based on nones.
+    'fill' will enter a None
+    'string' will enter 'None'
+    'trim' is valid and will do nothing.
+    Other values will raise assertion error.'''
+    if nones == 'fill':
+        row[key] = None
+    elif nones == 'string':
+        row[key] = 'None'
+    else:
+        assert nones == 'trim', f'Unknown interpretation of None: {nones}'
+
+def row_to_dict(key_row, row, *funcs, nones='fill'):
+    '''Takes a row of a data from a spreadsheet (list), converts to a dict.'''
+    out = {}
+    print(key_row)
+    print(row)
+    print(funcs)
+    print(nones)
+    for i,e in enumerate(key_row):
+        # Is the examined element of the row populated?
+        if len(row)-1 >= i:
+            if row[i] is None:
+                interpret_none(e, row, nones)
+            else:
+                # Does the examined element of the row have a function?
+                if len(funcs)-1 >= i:
+                    out[e] = funcs[i](row[i])
+        else:
+            # If not, it can be assumed to be None
+            interpret_none(e, row, nones)
+    return out                
+    
+def rows_to_list_of_dicts(sheet, *funcs, nones='fill'):
     '''Outputs a list of dicts from a spreadsheet, accepting functions to change the elements of the dicts.
     First row is labels and is untouched. If number of elements exceeds the functions provided, the rest are just handled as strings.
-    Nones by default are...'''
-    assert False, 'Incomplete.'
-    # nones fill = give it a None
-    # nones trim = leave it out of the dict, rather than adding 'None'
-    # nones string = 'None'
+    Nones by default are "fill" (with None), "trim" (exclude from the dict), and "string" ("None")...'''
     out = []
     first_row = sheet[0]
     for row in sheet[1:]:
-        record = {}
-        if not funcs:
-            for i,e in enumerate(row):
-                if trim_None:
-                    if e is not None:
-                        record[first_row[i]] = e
-                else:
-                    record[first_row[i]] = e
-        else:
-            for i,e in enumerate(row):
-                if trim_None:
-                    if e is not None:
-                        if i+1 <= len(funcs):
-                            func = funcs[i]
-                        else:
-                            func = str
-                else:
-                    if i+1 <= len(funcs):
-                        func = funcs[i]
-                    else:
-                        func = str
-                record[first_row[i]] = func(e)
-        out.append(record)
+        out.append(row_to_dict(first_row, row, *funcs, nones=nones))
     return out
     
 def dict_of_dicts_from_list_of_dicts(key, dicts):
