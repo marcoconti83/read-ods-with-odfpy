6e02b12fa62d742a22e1aa370f88e30ef5cc7b6a
InterestsFantastic <30320765+InterestsFantastic@users.noreply.github.com>
2022-10-13 14:34:25 -0400

    Now allows the creation of a dict of objects... instead of just a dict of dicts.

--- utils.py
+++ utils.py
@@ -18,6 +18,27 @@ def keyval_sheet_to_dict(sheet, sheetname, *funcs):
                 out[funcs[0](row[0])] = funcs[1](row[1])
     return out
 
+def dict_of_dicts_to_dict_of_objs(dict_of_dicts, objclass):
+    '''Given a dict of dicts, outputs a dict of objects using the dict_of_dicts values.items as **kwargs.'''
+    out = {}
+    for (k,v) in dict_of_dicts.items():
+        out[k] = objclass(**v)
+    return out
+
+def dict_sheet_to_dict_of_objs(sheet, sheetname, key, objclass, *funcs, nones='fill'):
+    '''Creates a dict of objects for a particular sheet in an ODSReader() object.
+    sheet is an ODSReader().
+    sheetname is the worksheet name.
+    key is the column that should be the key of the new dict of objs
+    objclass is the class that will be called via __init__(**kwargs), with kwargs populated from the rows.
+    funcs are functions that should be applied to the data as it becomes entries in the dict.
+    nones describes how to handle empty fields. 'fill' fills with None, 'trim' removes, 'string' fills with 'None'.'''
+    out = sheet.getSheet(sheetname)
+    out = rows_to_list_of_dicts(out, *funcs, nones=nones)
+    out = dict_of_dicts_from_list_of_dicts(key, out)
+    out = dict_of_dicts_to_dict_of_objs(out, objclass)
+    return out
+
 def interpret_none(key, interpreted_dict, nones='fill'):
     '''Enters a value into row[key] based on nones.
     'fill' will enter a None
